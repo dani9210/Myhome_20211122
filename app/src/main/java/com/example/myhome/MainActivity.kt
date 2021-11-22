7c9a921d5dcf95f4ccdac1b537691bc098c024ef
김다은 <softeun92@naver.com>
2021-11-22 20:19:22 +0900

    로그인 버튼이 이벤트 처리   - 레트로핏 활용 예정

--- app/src/main/java/com/example/myhome/MainActivity.kt
+++ app/src/main/java/com/example/myhome/MainActivity.kt
@@ -18,6 +18,18 @@ class MainActivity : BaseActivity() {
 
     override fun setupEvents() {
 
+        binding.btnLogin.setOnClickListener {
+
+
+//            1. 입력한 id/pw 변수담자
+
+            val inputEmail = binding.edtEmail.text.toString()
+            val inputPassword = binding.edtPassword.text.toString()
+
+//            2. 서버에 로그인 API 호출
+
+        }
+
     }
 
     override fun setValues() {
